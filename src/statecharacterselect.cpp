eb50c1f90635d4398f2bc4e01bceb28e2e364bf1
Alex Shelley <alexshelley@brbmoon.com>
2013-09-27 20:04:43 +0900

    Adding the rest of the race texts

--- src/statecharacterselect.cpp
+++ src/statecharacterselect.cpp
@@ -170,6 +170,9 @@ void State_CharacterSelect::Update(Stack<iGameState*> *s, World *w, char c){
 			if( _name.size() < 20 )
 				_name+= c;
 			break;
+		case BackStory:
+			s->Replace(new State_GameMode());
+			break;
 	}
 	
 }
@@ -292,12 +295,28 @@ void State_CharacterSelect::_SelectRaceRender(Renderer *r){
 			raceCategoryName = "Mystical";
 			race1[0] = "[1] Human";
 			race1[1] = "As typical as they come, in all shapes and sizes.\nUPPER-CLASS: Starts wih an additional 5 EXTRA MP\nHOMEOSTASIS: Generates +2 to ABILITY meter per turn\nBOOTSTRAPS:  Heals self upon enemy MISS/STUNNED (INT)";
+			race2[0] = "[2] Silian";
+			race2[1] = "Machine, robot, and biological; one of these they are not.\nDICE ROLL:   When using HEAL, grants 3%%%% chance to fully heal\nOVERFLOW:    HP regenerates when ULT meter is charged (VIT)\nAEGIS CHIP:  Heals self upon REPOSITIONING (INT)";
+			race3[0] = "[3] Kraste";
+			race3[1] = "A nomadic race of ice people, they love to procreate often.\nICE COMPLEX: Grants 10%%%% chance to freeze enemies\nNIGHTSHADE:  Deals bonus damage per turn to enemy (INT)\nSENESCENCE:  Enemy takes damage equal to your MP per turn";
 			break;
 		case Powerful:
 			raceCategoryName = "Powerful";
+			race1[0] = "[1] Flammkin";
+			race1[1] = "Charred with hellfire, these firebreathers relish in blood.\nDRY HEAT:    When using HEAL, 5%%%% chance to BLEED enemies\nBRICK OVEN:  Grants immunity to MOLTEN and VAMPIRIC\nDEADLY VIEW: Using your ULTIMATE will BLEED enemies";
+			race2[0] = "[2] Jotunnar";
+			race2[1] = "Dwarven inhabitants of Jotun, these men truly enjoy pain.\nIRON MAIDEN: Enemies take damage when player is BLEEDING\nFURY WHIP:   ULTIMATE deals bonus damage to weakened enemies\nPAINBRINGER: When HP is low, 10%%%% chance to +10%%%% MAX HP";
+			race3[0] = "[3] Wyspera";
+			race3[1] = "These dark shadow creatures are truly one with nature.\nSHADOW RUN:  Dodge chance is passively increased by 5%%%%\nDARKLING:    ULTIMATE damage increases with level (DEX)\nWALLFLOWER:  Grants +5 bonus MP during REPOSITION";
 			break;
 		case Steadfast:
 			raceCategoryName = "Steadfast";
+			race1[0] = "[1] Goern";
+			race1[1] = "These tough amphibians reside on land, but love the water.\nIRON SCALES: Grants 50%%%% resistance against BLEEDING\nHARD SELL:   Using HEAL will heal a bonus amount (DEX)\nEVASIVE TOE: Dodge chance increases when player HP is lowered";
+			race2[0] = "[2] Sonite";
+			race2[1] = "At a glance, their skin could be mistaken for soft stone.\nTENACITY:    HEAL is more effective in prolonged battles\nABSORBANT:   Using HEAL temporarily increases MAX HP (VIT)\nCRUMPLE:     Reduce enemy CHARGE damage by 40%%%%";
+			race3[0] = "[3] Huskian";
+			race3[1] = "Standing at over eight feet tall, these giants are unshakable.\nLIFELINK:    Grants 5%%%% chance per turn to heal 25%%%% of MAX HP\nBLOODSCENT:  ABILITY meter +10 if you or enemy is bleeding\nGUARD CRUSH: Has 33%%%% chance to break GUARDING enemy per turn";
 			break;
 	}
 	r->printlns(0,  "+-----------------------------------------------------------------------------+");
@@ -305,10 +324,10 @@ void State_CharacterSelect::_SelectRaceRender(Renderer *r){
 	r->printlns(0,  "+-----------------------------------------------------------------------------+");
 	r->prints(3,5,  race1[0]);
 	r->prints(17,5, race1[1].c_str());
-	r->prints(3,10, "");
-	r->prints(17,10,"");
-	r->prints(3,15, "");
-	r->prints(17,15,"");
+	r->prints(3,10, race2[0]);
+	r->prints(17,10,race2[1].c_str());
+	r->prints(3,15, race3[0]);
+	r->prints(17,15,race3[1].c_str());
 	r->prints(3,21, "[A] Back");
 	r->printlns(0,  "");
 	r->printlns(0,  "+-----------------------------------------------------------------------------+");
