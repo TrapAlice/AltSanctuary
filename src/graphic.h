585d40a1dcaa0c1ef0123d4f5844a5ae699e3413
Alex Shelley <alexshelley@brbmoon.com>
2013-09-29 22:34:41 +0900

    Moving all graphics to their own files

---
+++ src/graphic.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+
+enum class Graphic{
+	BARBARIAN_M,
+	BARBARIAN_F,
+	PALADIN_M,
+	PALADIN_F,
+	ASSASSIN_M,
+	ASSASSIN_F,
+	WIZARD_M,
+	WIZARD_F,
+	DRUID_M,
+	DRUID_F,
+	RANGER_M,
+	RANGER_F,
+	BLANK_FACE,
+	DIANE,
+	num_graphics
+};
+
+void        GRAPHIC_INIT();
+std::string GRAPHIC(Graphic g);
